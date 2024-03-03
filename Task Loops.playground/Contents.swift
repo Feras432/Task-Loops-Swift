07c2f1c61903606279ba843a63572e5d18e074bf
Feras Alshadad <ferasalshadad@Ferass-Laptop.local>
2024-03-03 15:32:50 +0300

    Added files

---
+++ Task Loops.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//Task: Loops Task 1
+var favoriteFruits = ["Apple", "Banana", "Cherry"]
+for favoriteFruit in favoriteFruits {
+    print(favoriteFruit)
+}
+
+//Task: Loops Task 2
+for i in 1...5{
+    for j in 1...5{
+        print("The outcome of the multiplication of",i,"and",j,"is", i*j)
+    }
+}
+
